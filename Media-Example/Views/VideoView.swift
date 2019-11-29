c9a6df20064098662f0cde844af242df594c1055
Christian Elies <chris.elies13@gmail.com>
2019-11-29 20:06:33 +0100

    feat(video): export example

--- Media-Example/Views/VideoView.swift
+++ Media-Example/Views/VideoView.swift
@@ -11,9 +11,34 @@ import Media
 import SwiftUI
 
 struct VideoView: View {
+    @State private var exportSuccessful = false
+
     let video: Video
 
     var body: some View {
         video.view
+            .navigationBarItems(trailing: Button(action: {
+                guard let url = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else {
+                    return
+                }
+
+                let fileURL = url.appendingPathComponent("\(UUID().uuidString)")
+                guard let exportDestination = try? Video.ExportDestination(url: fileURL, fileType: .mobile3GPP2) else {
+                    return
+                }
+
+                self.exportSuccessful = false
+                self.video.export(to: exportDestination, quality: .low) { result in
+                    switch result {
+                    case .success:
+                        self.exportSuccessful = true
+                    case .failure:
+                        self.exportSuccessful = false
+                    }
+                }
+            }) {
+                Text("Export")
+                    .foregroundColor(exportSuccessful ? Color(.systemGreen) : Color(.systemBlue))
+            })
     }
 }
