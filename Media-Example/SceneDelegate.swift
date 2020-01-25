c5eff879af7ee1010574b103d5366debeb9e6ec2
Christian Elies <chris.elies13@gmail.com>
2020-01-26 00:02:06 +0100

    refactor(): removed availability observer logic for now

--- Media-Example/SceneDelegate.swift
+++ Media-Example/SceneDelegate.swift
@@ -11,16 +11,12 @@ import UIKit
 import SwiftUI
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-    private var photoLibraryAvailabilityObserver: ExamplePhotoLibraryAvailabilityObserver?
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-
-        photoLibraryAvailabilityObserver = ExamplePhotoLibraryAvailabilityObserver()
-        Media.availabilityObserver = photoLibraryAvailabilityObserver
 
         // Create the SwiftUI view that provides the window contents.
         let contentView = ContentView()
