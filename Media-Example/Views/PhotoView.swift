54506055d8adaa7d3f19227479c94a70336e12d7
Christian Elies <chris.elies13@gmail.com>
2021-02-13 23:31:43 +0100

    fix(photo view): fixed properties and share sheet

--- Media-Example/Views/PhotoView.swift
+++ Media-Example/Views/PhotoView.swift
@@ -9,97 +9,97 @@
 import MediaCore
 import SwiftUI
 
+extension Photo.Properties: Identifiable {
+    public var id: String { "\(exif.dateTimeOriginal ?? "")_\(UUID().uuidString)" }
+}
+
+extension Data: Identifiable {
+    public var id: Self { self }
+}
+
 struct PhotoView: View {
     let photo: Photo
 
     @State private var data: Data?
     @State private var error: Error?
-    @State private var isShareSheetVisible = false
-    @State private var isPropertiesSheetVisible = false
     @State private var isErrorAlertVisible = false
     @State private var isFavorite = false
     @State private var properties: Photo.Properties?
 
     var body: some View {
-        if data == nil && error == nil {
-            photo.data { result in
-                switch result {
-                case .success(let data):
-                    self.data = data
-                case .failure(let error):
-                    self.error = error
-                }
-            }
-        }
-
-        return VStack(spacing: 16) {
+        VStack(spacing: 16) {
             photo.view { image in
                 image
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .onTapGesture {
-                        if self.properties != nil {
-                            self.isPropertiesSheetVisible = true
-                        }
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            }
+            .onAppear {
+                isFavorite = photo.metadata?.isFavorite ?? false
+            }
+            .onTapGesture {
+                photo.properties { result in
+                    switch result {
+                    case .success(let properties):
+                        self.properties = properties
+                    case .failure:
+                        properties = nil
                     }
-                    .sheet(isPresented: self.$isPropertiesSheetVisible, onDismiss: {
-                        self.isPropertiesSheetVisible = false
-                    }) {
-                        self.properties.map { properties in
-                            List {
-                                Section {
-                                    Text(String(describing: properties.exif))
-                                }
-
-                                Section {
-                                    Text(String(describing: properties.gps))
-                                }
-
-                                Section {
-                                    Text(String(describing: properties.tiff))
-                                }
-                            }.listStyle(GroupedListStyle())
-                        }
+                }
+            }
+            .sheet(item: $properties, onDismiss: {
+                properties = nil
+            }) { properties in
+                List {
+                    Section {
+                        Text(String(describing: properties.exif))
                     }
+
+                    Section {
+                        Text(String(describing: properties.gps))
+                    }
+
+                    Section {
+                        Text(String(describing: properties.tiff))
+                    }
+                }.listStyle(GroupedListStyle())
             }
 
             Text(photo.subtypes.map { String(describing: $0) }.joined(separator: ", "))
-        }.onAppear {
-            self.isFavorite = self.photo.metadata?.isFavorite ?? false
-
-            self.photo.properties { result in
-                switch result {
-                case .success(let properties):
-                    self.properties = properties
-                case .failure:
-                    self.properties = nil
-                }
-            }
-        }.navigationBarItems(trailing: HStack {
-            self.photo.metadata.map { metadata in
+        }
+        .navigationBarItems(trailing: HStack {
+            if let metadata = photo.metadata {
                 Button(action: {
-                    self.photo.favorite(!metadata.isFavorite) { result in
+                    photo.favorite(!metadata.isFavorite) { result in
                         switch result {
                         case .success:
-                            self.isFavorite = self.photo.metadata?.isFavorite ?? false
+                            isFavorite = photo.metadata?.isFavorite ?? false
                         case .failure(let error):
                             self.error = error
-                            self.isErrorAlertVisible = true
+                            isErrorAlertVisible = true
                         }
                     }
                 }) {
                     Image(systemName: isFavorite ? "heart.fill" : "heart")
-                }.alert(isPresented: $isErrorAlertVisible) { self.errorAlert(error) }
+                }.alert(isPresented: $isErrorAlertVisible) { errorAlert(error) }
             }
 
             Button(action: {
-                self.isShareSheetVisible = true
+                photo.data { result in
+                    switch result {
+                    case .success(let data):
+                        self.data = data
+                    case .failure(let error):
+                        self.error = error
+                    }
+                }
             }) {
                 Text("Share")
-            }.sheet(isPresented: $isShareSheetVisible, onDismiss: {
-                self.isShareSheetVisible = false
-            }) {
-                self.data.map { UIImage(data: $0).map { ActivityView(activityItems: [$0], applicationActivities: []) } }
+            }.sheet(item: $data, onDismiss: {
+                data = nil
+            }) { data in
+                if let image = UIImage(data: data) {
+                    ActivityView(activityItems: [image], applicationActivities: [])
+                }
             }
         })
     }
@@ -108,7 +108,7 @@ struct PhotoView: View {
 extension PhotoView {
     private func errorAlert(_ error: Error?) -> Alert {
         Alert(title: Text("Error"), message: Text(error?.localizedDescription ?? "An unknown error occurred"), dismissButton: .cancel {
-            self.isErrorAlertVisible = false
+            isErrorAlertVisible = false
         })
     }
 }
