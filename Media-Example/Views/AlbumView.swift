96f5773d466e6ee798a254412728530744dfe6ce
Christian Elies <chris.elies13@gmail.com>
2021-02-17 00:07:52 +0100

    refactor(): removed unnecessary availability annotation; refactor(album view): show total number of media items below the navigation title

--- Media-Example/Views/AlbumView.swift
+++ Media-Example/Views/AlbumView.swift
@@ -13,40 +13,42 @@ struct AlbumView: View {
     let album: Album
 
     var body: some View {
-        List {
-            Section {
-                NavigationLink(destination: AudiosView(audios: album.audios)) {
-                    Text("\(album.audios.count) audios")
+        VStack(spacing: 0) {
+            Text("\(album.allMedia.count) media items").font(.footnote).padding(.vertical)
+
+            List {
+                Section {
+                    NavigationLink(destination: AudiosView(audios: album.audios)) {
+                        Text("\(album.audios.count) audios")
+                    }
+                }
+
+                Section {
+                    NavigationLink(destination: LivePhotosView(livePhotos: album.livePhotos)) {
+                        Text("\(album.livePhotos.count) live photos")
+                    }
+                }
+
+                Section {
+                    NavigationLink(destination: PhotosView(photos: album.photos)) {
+                        Text("\(album.photos.count) photos")
+                    }
+                }
+
+                Section {
+                    NavigationLink(destination: PhotoGridView(photos: album.photos)) {
+                        Text("Photo GridView")
+                    }
+                }
+
+                Section {
+                    NavigationLink(destination: VideosView(videos: album.videos)) {
+                        Text("\(album.videos.count) videos")
+                    }
                 }
             }
-
-            Section {
-                NavigationLink(destination: LivePhotosView(livePhotos: album.livePhotos)) {
-                    Text("\(album.livePhotos.count) live photos")
-                }
-            }
-
-            Section {
-                NavigationLink(destination: PhotosView(photos: album.photos)) {
-                    Text("\(album.photos.count) photos")
-                }
-            }
-
-            Section {
-                NavigationLink(destination: PhotoGridView(photos: album.photos)) {
-                    Text("Photo GridView")
-                }
-            }
-
-            Section {
-                NavigationLink(destination: VideosView(videos: album.videos)) {
-                    Text("\(album.videos.count) videos")
-                }
-            }
-
-            Text("\(album.allMedia.count) media items")
+            .listStyle(InsetGroupedListStyle())
         }
-        .listStyle(InsetGroupedListStyle())
         .navigationBarTitle(Text(album.localizedTitle ?? ""), displayMode: .inline)
     }
 }
--- Media-Example/Views/PhotoGridView.swift
+++ Media-Example/Views/PhotoGridView.swift
@@ -13,20 +13,18 @@ struct PhotoGridView: View {
     let photos: [Photo]
 
     var body: some View {
-        if #available(iOS 14, macOS 11, tvOS 14, *) {
-            let columns: [GridItem] = Array(repeating: .init(.flexible()), count: 3)
+        let columns: [GridItem] = Array(repeating: .init(.flexible()), count: 3)
 
-            ScrollView {
-                LazyVGrid(columns: columns, content: {
-                    ForEach(photos) { photo in
-                        photo.view(targetSize: .init(width: 200, height: 200)) { image in
-                            image
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                        }
+        ScrollView {
+            LazyVGrid(columns: columns, content: {
+                ForEach(photos) { photo in
+                    photo.view(targetSize: .init(width: 200, height: 200)) { image in
+                        image
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
                     }
-                })
-            }
+                }
+            })
         }
     }
 }
