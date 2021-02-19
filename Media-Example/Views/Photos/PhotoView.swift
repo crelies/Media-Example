3ed2ba1207168c5f494b7cd1c36fd496d4f42121
Christian Elies <chris.elies13@gmail.com>
2021-02-19 23:05:19 +0100

    refactor(photo view): improvements

--- Media-Example/Views/Photos/PhotoView.swift
+++ Media-Example/Views/Photos/PhotoView.swift
@@ -51,62 +51,27 @@ struct PhotoView: View {
                     }
                 }
             }
-            .sheet(item: $properties, onDismiss: {
-                properties = nil
-            }) { properties in
-                if #available(iOS 14.0, *) {
-                    if let location = properties.gps.location {
-                        Map(coordinateRegion: .constant(.init(center: .init(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude), latitudinalMeters: 5000, longitudinalMeters: 5000)), annotationItems: [location.coordinate]) { item in
-                            MapMarker(coordinate: item)
-                        }
-                    }
-                }
-
-                List {
-                    Section {
-                        Text(String(describing: properties.exif))
-                    }
-
-                    Section {
-                        Text(String(describing: properties.gps))
-                    }
-
-                    Section {
-                        Text(String(describing: properties.tiff))
-                    }
-                }
-                .listStyle(InsetGroupedListStyle())
-            }
+            .sheet(
+                item: $properties,
+                onDismiss: {
+                    properties = nil
+                },
+                content: propertiesScreen
+            )
 
             Text(photo.subtypes.map { String(describing: $0) }.joined(separator: ", "))
         }
         .navigationBarItems(trailing: HStack {
             if let metadata = photo.metadata {
                 Button(action: {
-                    photo.favorite(!metadata.isFavorite) { result in
-                        switch result {
-                        case .success:
-                            isFavorite = photo.metadata?.isFavorite ?? false
-                        case .failure(let error):
-                            self.error = error
-                            isErrorAlertVisible = true
-                        }
-                    }
+                    toggleFavoriteState(isFavorite: metadata.isFavorite)
                 }) {
                     Image(systemName: isFavorite ? "heart.fill" : "heart")
-                }.alert(isPresented: $isErrorAlertVisible) { errorAlert(error) }
+                }
+                .alert(isPresented: $isErrorAlertVisible) { errorAlert(error) }
             }
 
-            Button(action: {
-                photo.data { result in
-                    switch result {
-                    case .success(let data):
-                        self.data = data
-                    case .failure(let error):
-                        self.error = error
-                    }
-                }
-            }) {
+            Button(action: share) {
                 Text("Share")
             }.sheet(item: $data, onDismiss: {
                 data = nil
@@ -120,9 +85,71 @@ struct PhotoView: View {
 }
 
 private extension PhotoView {
+    @ViewBuilder func propertiesScreen(_ properties: Photo.Properties) -> some View {
+        if #available(iOS 14.0, *) {
+            if let location = properties.gps.location {
+                Map(
+                    coordinateRegion: .constant(
+                        .init(
+                            center: .init(
+                                latitude: location.coordinate.latitude,
+                                longitude: location.coordinate.longitude
+                            ),
+                            latitudinalMeters: 5000,
+                            longitudinalMeters: 5000
+                        )
+                    ),
+                    annotationItems: [location.coordinate]
+                ) { item in
+                    MapMarker(coordinate: item)
+                }
+            }
+        }
+
+        List {
+            Section {
+                Text(String(describing: properties.exif))
+            }
+
+            Section {
+                Text(String(describing: properties.gps))
+            }
+
+            Section {
+                Text(String(describing: properties.tiff))
+            }
+        }
+        .listStyle(InsetGroupedListStyle())
+    }
+
     func errorAlert(_ error: Error?) -> Alert {
         Alert(title: Text("Error"), message: Text(error?.localizedDescription ?? "An unknown error occurred"), dismissButton: .cancel {
             isErrorAlertVisible = false
         })
+    }
+}
+
+private extension PhotoView {
+    func share() {
+        photo.data { result in
+            switch result {
+            case .success(let data):
+                self.data = data
+            case .failure(let error):
+                self.error = error
+            }
+        }
+    }
+
+    func toggleFavoriteState(isFavorite: Bool) {
+        photo.favorite(!isFavorite) { result in
+            switch result {
+            case .success:
+                self.isFavorite = photo.metadata?.isFavorite ?? false
+            case .failure(let error):
+                self.error = error
+                isErrorAlertVisible = true
+            }
+        }
     }
 }
